5c68045b99face93b319e7d5bb5be86f170c9e99
x10forevers <android.x10f@gmail.com>
2014-03-15 19:44:11 -0400

    We do not need Term.apk prebuilt

--- config/common.mk
+++ config/common.mk
@@ -202,11 +202,6 @@ PRODUCT_PACKAGES += \
     Superuser \
     su
 
-# Terminal Emulator
-PRODUCT_COPY_FILES +=  \
-    vendor/jafier/proprietary/Term.apk:system/app/Term.apk \
-    vendor/jafier/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
-
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.root_access=1
 else
